b8f2af8ed2c50bfbd40783a4f3d21b00439eb8f8
David Peatman <davidpeatman@pm.me>
2020-03-02 22:38:51 -0800

    Refactoring mesh generation to use single mesh instead of multiple game objects
    By having duplicate vertices, each cube can be relative to its own 8 vertices, simplifying generation code

--- Assets/Scripts/Cubes.cs
+++ Assets/Scripts/Cubes.cs
@@ -6,9 +6,8 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 public class Cubes : MonoBehaviour {
 
-    private Mesh[] meshes;
+    private Mesh mesh;
 
-    private Dictionary<string, GameObject> cubeObjects;
 
     private int width;
     private int length;
@@ -25,76 +24,55 @@ public class Cubes : MonoBehaviour {
     }
 
     void Update() {
-        //UpdateMeshes();
+        Reset(width, length);
     }
 
     public void Reset(int width, int length) {
-        if (cubeObjects == null) {
-            cubeObjects = new Dictionary<string, GameObject>();
-        }
-
-        foreach (KeyValuePair<string, GameObject> entry in cubeObjects) {
-            if (entry.Value != null) {
-                entry.Value.SetActive(false);
-            }
-        }
-
         this.width = width;
         this.length = length;
-
-        meshes = new Mesh[length * width];
+        if (mesh == null) {
+            mesh = new Mesh();
+        } else {
+            mesh.Clear();
+        }
+        GetComponent<MeshFilter>().mesh = mesh;
 
         Generate();
     }
 
     public void Generate() {
-        CleanUp();
-        CreateObjects();
-        UpdateMeshes();
-        Combine();
+        CreateMesh();
     }
 
     public void CleanUp() {
-        Component[] meshFilters = GetComponentsInChildren<MeshFilter>();
-        for (int i = 0; i < meshFilters.Length; i++) {
-            ((MeshFilter)meshFilters[i]).sharedMesh.Clear();
-        }
-        GetComponent<MeshFilter>().sharedMesh.Clear();
     }
 
-    void CreateObjects() {
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < width; j++) {
-                int index = i * width + j;
-                string name = "Cube-" + i + "-" + j;
-                GameObject cube;
-                Mesh mesh = new Mesh();
-                if (!cubeObjects.ContainsKey(name) || cubeObjects[name] == null) {
-                    cube = new GameObject(name);
-                    cube.transform.SetParent(this.transform);
-                    cube.AddComponent<MeshFilter>().mesh = mesh;
-                    cubeObjects.Add(name, cube);
-                } else {
-                    cube = cubeObjects[name];
-                    cube.GetComponent<MeshFilter>().mesh = mesh;
-                    cube.SetActive(true);
-                }
-                meshes[index] = mesh;
-            }
-        }
-    }
+    void CreateMesh() {
+        Vector3[] vertices = new Vector3[width * length * 8];
+        int trianglesPerCube = 12 * 3;
+        int[] triangles = new int[width * length * trianglesPerCube];
 
-    void UpdateMeshes() {
+        int cubeIndex = 0;
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < width; j++) {
-                int index = i * width + j;
-                Mesh mesh = meshes[index];
-                mesh.Clear();
                 Vector3 origin = new Vector3(-width / 2f + j, 0, -length / 2f + i);
-                mesh.vertices = CreateCubeVertices(origin);
-                mesh.triangles = CreateCubeTriangles(0);
+
+                Vector3[] cubeVertices = CreateCubeVertices(origin);
+                for (int x = 0; x < 8; x++) {
+                    vertices[cubeIndex * 8 + x] = cubeVertices[x];
+                }
+
+                int[] cubeTriangles = CreateCubeTriangles(cubeIndex * 8);
+                for (int x = 0; x < trianglesPerCube; x++) {
+                    triangles[cubeIndex * trianglesPerCube + x] = cubeTriangles[x];
+                }
+
+                cubeIndex++;
             }
         }
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
     }
 
     Vector3[] CreateCubeVertices(Vector3 origin) {
@@ -128,30 +106,13 @@ public class Cubes : MonoBehaviour {
         };
     }
 
-    void Combine() {
-        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
-        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
-
-        for (int i = 0; i < meshFilters.Length; i++) {
-            combine[i].mesh = meshFilters[i].sharedMesh;
-            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
-            meshFilters[i].gameObject.SetActive(false);
-        }
-        transform.GetComponent<MeshFilter>().mesh = new Mesh();
-        transform.GetComponent<MeshFilter>().sharedMesh.CombineMeshes(combine);
-        transform.gameObject.SetActive(true);
-    }
-
     void OnDrawGizmos() {
-        if (meshes == null || !Application.isPlaying) {
+        if (mesh == null) {
             return;
         }
 
-        for (int i = 0; i < meshes.Length; i++) {
-            Mesh mesh = meshes[i];
-            for (int j = 0; j < meshes[i].vertices.Length; j++) {
-                Gizmos.DrawSphere(mesh.vertices[j], .05f);
-            }
+        for (int j = 0; j < mesh.vertices.Length; j++) {
+            Gizmos.DrawSphere(mesh.vertices[j], .05f);
         }
     }
 }
