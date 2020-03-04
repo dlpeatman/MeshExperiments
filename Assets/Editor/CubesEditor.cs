eb03336d732e71d340d1f6d6a7ebba7df0feeeaf
David Peatman <davidpeatman@pm.me>
2020-03-04 08:16:13 -0800

    Increasing max grid sizes to 50

--- Assets/Editor/CubesEditor.cs
+++ Assets/Editor/CubesEditor.cs
@@ -9,9 +9,9 @@ public class CubesEditor : Editor {
 
         Cubes cubes = (Cubes)target;
         bool gizmos = EditorGUILayout.Toggle("Draw Gizmos", cubes.DrawGizmos);
-        int width = EditorGUILayout.IntSlider("Width", cubes.Width, 1, 20);
-        int length = EditorGUILayout.IntSlider("Length", cubes.Length, 1, 20);
-        int height = EditorGUILayout.IntSlider("Height", cubes.Height, 1, 20);
+        int width = EditorGUILayout.IntSlider("Width", cubes.Width, 1, 50);
+        int length = EditorGUILayout.IntSlider("Length", cubes.Length, 1, 50);
+        int height = EditorGUILayout.IntSlider("Height", cubes.Height, 1, 50);
         float xNoiseOffset = EditorGUILayout.Slider("X Noise Offset", cubes.XNoiseOffset, 0, 10);
         float yNoiseOffset = EditorGUILayout.Slider("Y Noise Offset", cubes.YNoiseOffset, 0, 10);
         float zNoiseOffset = EditorGUILayout.Slider("Z Noise Offset", cubes.ZNoiseOffset, 0, 10);
