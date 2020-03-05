b257e687aa142a25596f96fe0a82a73f29fb4981
David Peatman <davidpeatman@pm.me>
2020-03-04 22:19:05 -0800

    Adding looped chunk generation and adding layers to perlin noise

--- Assets/Editor/CubesEditor.cs
+++ Assets/Editor/CubesEditor.cs
@@ -8,17 +8,20 @@ public class CubesEditor : Editor {
     public override void OnInspectorGUI() {
 
         Cubes cubes = (Cubes)target;
-        int width = EditorGUILayout.IntSlider("Width", cubes.Width, 1, 20);
-        int length = EditorGUILayout.IntSlider("Length", cubes.Length, 1, 20);
-        int height = EditorGUILayout.IntSlider("Height", cubes.Height, 1, 12);
-        float xNoiseOffset = EditorGUILayout.Slider("X Noise Offset", cubes.XNoiseOffset, 0, 10);
-        float yNoiseOffset = EditorGUILayout.Slider("Y Noise Offset", cubes.YNoiseOffset, 0, 10);
-        float zNoiseOffset = EditorGUILayout.Slider("Z Noise Offset", cubes.ZNoiseOffset, 0, 10);
-        float scale = EditorGUILayout.Slider("Scale", cubes.Scale, .01f, .15f);
-        float isoLevel = EditorGUILayout.Slider("IsoLevel", cubes.IsoLevel, 0f, 1f);
+        cubes.width = EditorGUILayout.IntSlider("Width", cubes.width, 1, 16);
+        cubes.length = EditorGUILayout.IntSlider("Length", cubes.length, 1, 16);
+        cubes.height = EditorGUILayout.IntSlider("Height", cubes.height, 1, 16);
+        cubes.xNoiseOffset = EditorGUILayout.Slider("X Noise Offset", cubes.xNoiseOffset, 0, 10);
+        cubes.yNoiseOffset = EditorGUILayout.Slider("Y Noise Offset", cubes.yNoiseOffset, 0, 10);
+        cubes.zNoiseOffset = EditorGUILayout.Slider("Z Noise Offset", cubes.zNoiseOffset, 0, 10);
+        cubes.scale = EditorGUILayout.Slider("Scale", cubes.scale, .01f, .15f);
+        cubes.isoLevel = EditorGUILayout.Slider("IsoLevel", cubes.isoLevel, 0f, 1f);
+        cubes.octaves = EditorGUILayout.IntSlider("Octaves", cubes.octaves, 1, 4);
+        cubes.lacunarity = EditorGUILayout.Slider("Lacunarity", cubes.lacunarity, 1f, 6f);
+        cubes.persistance = EditorGUILayout.Slider("Persistance", cubes.persistance, .01f, 1f);
 
         if (GUI.changed) {
-            cubes.Reset(width, length, height, xNoiseOffset, yNoiseOffset, zNoiseOffset, scale, isoLevel);
+            cubes.Reset();
         }
     }
 }
--- Assets/Scripts/Cubes.cs
+++ Assets/Scripts/Cubes.cs
@@ -5,28 +5,23 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 public class Cubes : MonoBehaviour {
 
-    private Dictionary<Vector3, Chunk> chunkDictionary;
+    private Dictionary<string, Chunk> chunkDictionary;
 
-    private int width;
-    private int length;
-    private int height;
-    private float xNoiseOffset;
-    private float yNoiseOffset;
-    private float zNoiseOffset;
-    private float scale;
-    private float isoLevel;
+    public int width;
+    public int length;
+    public int height;
+    public float xNoiseOffset;
+    public float yNoiseOffset;
+    public float zNoiseOffset;
+    public float scale;
+    public float isoLevel;
 
-    public int Width { get => width; }
-    public int Length { get => length; }
-    public int Height { get => height; set => height = value; }
-    public float XNoiseOffset { get => xNoiseOffset; set => xNoiseOffset = value; }
-    public float YNoiseOffset { get => yNoiseOffset; set => yNoiseOffset = value; }
-    public float ZNoiseOffset { get => zNoiseOffset; set => zNoiseOffset = value; }
-    public float Scale { get => scale; set => scale = value; }
-    public float IsoLevel { get => isoLevel; set => isoLevel = value; }
+    public int octaves;
+    public float lacunarity;
+    public float persistance;
 
     public Cubes(int width, int length, int height, float xNoiseOffset, float yNoiseOffset, float zNoiseOffset, float scale, float isoLevel) {
-        Reset(width, length, height, xNoiseOffset, yNoiseOffset, zNoiseOffset, scale, isoLevel);
+        Reset();
     }
 
     void Start() {
@@ -37,60 +32,61 @@ public class Cubes : MonoBehaviour {
         Generate();
     }
 
-    public void Reset(int width, int length, int height, float xNoiseOffset, float yNoiseOffset, float zNoiseOffset, float scale, float isoLevel) {
-        this.width = width;
-        this.length = length;
-        this.height = height;
-        this.xNoiseOffset = xNoiseOffset;
-        this.yNoiseOffset = yNoiseOffset;
-        this.zNoiseOffset = zNoiseOffset;
-        this.scale = scale;
-        this.isoLevel = isoLevel;
-
+    public void Reset() {
         if (chunkDictionary == null) {
-            chunkDictionary = new Dictionary<Vector3, Chunk>();
+            chunkDictionary = new Dictionary<string, Chunk>();
         }
+
+        CleanUp();
 
         Generate();
     }
 
     public void CleanUp() {
-        foreach (KeyValuePair<Vector3, Chunk> entry in chunkDictionary) {
-
+        Component[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        for (int i = 0; i < meshFilters.Length; i++) {
+            ((MeshFilter)meshFilters[i]).sharedMesh.Clear();
+            meshFilters[i].gameObject.SetActive(false);
         }
     }
 
     public void Generate() {
-        var origin = new Vector3(0, 0, 0);
-        var material = GetComponent<MeshRenderer>().sharedMaterial;
-        if (chunkDictionary.ContainsKey(origin)) {
-            var chunk = chunkDictionary[origin];
-            if (chunk == null || chunk.GameObject == null) {
-                chunkDictionary.Remove(origin);
-                var gameObject = new GameObject("Origin");
-                chunk = new Chunk(gameObject, material, transform);
-                chunkDictionary.Add(origin, chunk);
+        for (int z = 0; z < 3; z++) {
+            for (int x = 0; x < 3; x++) {
+                var name = "Chunk [" + x + ", " + z + "]";
+                var location = new Vector3(x * width, 0, z * length);
+                var material = GetComponent<MeshRenderer>().sharedMaterial;
+
+                Chunk chunk;
+                if (chunkDictionary.ContainsKey(name)) {
+                    chunk = chunkDictionary[name];
+                    if (chunk == null || chunk.GameObject == null) {
+                        chunkDictionary.Remove(name);
+                        var gameObject = new GameObject(name);
+                        chunk = new Chunk(gameObject, material, transform);
+                        chunkDictionary.Add(name, chunk);
+                    }
+                } else {
+                    var gameObject = new GameObject(name);
+                    chunk = new Chunk(gameObject, material, transform);
+                    chunkDictionary.Add(name, chunk);
+                }
+                chunk.GameObject.SetActive(true);
+                CreateMesh(location, chunk);
             }
-            CreateMesh(chunk);
-        } else {
-            var gameObject = new GameObject("Origin");
-            var chunk = new Chunk(gameObject, material, transform);
-            chunkDictionary.Add(origin, chunk);
-            CreateMesh(chunk);
         }
     }
 
-    void CreateMesh(Chunk chunk) {
+    void CreateMesh(Vector3 origin, Chunk chunk) {
         var mesh = chunk.Mesh;
         mesh.Clear();
 
         int trianglesPerCube = 4 * 3;
         var black = new Color32(0, 0, 0, 255);
         var white = new Color32(255, 255, 255, 255);
-        var chunkOrigin = new Vector3(0, 0, 0);
 
         Vector3[] vertices = new Vector3[width * length * height * 12];
-        // Color32[] colors = new Color32[width * length * height * 8];
+        Color32[] colors = new Color32[vertices.Length];
         int[] triangles = new int[width * length * height * trianglesPerCube];
 
         int cube = 0;
@@ -98,16 +94,15 @@ public class Cubes : MonoBehaviour {
         for (int y = 0; y < height; y++) {
             for (int z = 0; z < length; z++) {
                 for (int x = 0; x < width; x++, cube++) {
-                    Vector3 cubeOrigin = chunkOrigin + new Vector3(x, y, z);
+                    Vector3 cubeOrigin = origin + new Vector3(x, y, z);
                     int vertexOffset = cube * 12;
 
                     Vector3[] cubeVertices = CreateCubeVertices(cubeOrigin);
                     Triangulation.CubePoint[] cubePoints = new Triangulation.CubePoint[8];
                     for (int i = 0; i < 8; i++) {
                         Vector3 vertex = cubeVertices[i];
-                        float perlinValue = Mathf.PerlinNoise(xNoiseOffset + (vertex.x) * scale, zNoiseOffset + (vertex.z) * scale);
-                        cubePoints[i] = new Triangulation.CubePoint(vertex, perlinValue * (1 - vertex.y / (float)height));
-                        // colors[vertexOffset + i] = Color32.Lerp(black, white, perlinValue * (1 - y / (float)height));
+                        float isoValue = CalculateIsoValue(vertex);
+                        cubePoints[i] = new Triangulation.CubePoint(vertex, isoValue);
                     }
 
                     int cubeIndex = Triangulation.CalculateCubeIndex(isoLevel, cubePoints);
@@ -119,6 +114,7 @@ public class Cubes : MonoBehaviour {
 
                     Vector3[] interpolatedVertices = Triangulation.GetVertices(isoLevel, cubeIndex, cubePoints);
                     for (int v = 0; v < interpolatedVertices.Length; v++) {
+                        colors[vertexOffset + v] = Color32.Lerp(black, white, interpolatedVertices[v].y / height);
                         vertices[vertexOffset + v] = interpolatedVertices[v];
                     }
 
@@ -132,9 +128,35 @@ public class Cubes : MonoBehaviour {
         }
 
         mesh.vertices = vertices;
-        // mesh.colors32 = colors;
+        mesh.colors32 = colors;
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
+    }
+
+    float CalculateIsoValue(Vector3 vertex) {
+        System.Random prng = new System.Random(2);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++) {
+            float offsetX = prng.Next(-100000, 100000) + xNoiseOffset;
+            float offsetZ = prng.Next(-100000, 100000) + zNoiseOffset;
+            octaveOffsets[i] = new Vector2(offsetX, offsetZ);
+        }
+
+        float amplitude = 1;
+        float frequency = 1;
+        float noiseHeight = 0;
+
+        for (int i = 0; i < octaves; i++) {
+            float sampleX = vertex.x * scale * frequency + octaveOffsets[i].x;
+            float sampleY = vertex.z * scale * frequency + octaveOffsets[i].y;
+
+            float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+            noiseHeight += perlinValue * amplitude;
+
+            amplitude *= persistance;
+            frequency *= lacunarity;
+        }
+        return noiseHeight * (1 - vertex.y / height);
     }
 
 
