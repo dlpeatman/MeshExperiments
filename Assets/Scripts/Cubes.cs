0c9e045ef5beaf138a032419799647a178f4aace
David Peatman <davidpeatman@pm.me>
2020-03-06 20:48:41 -0800

    Moving random number generation to improve performance and adding more color to land

--- Assets/Scripts/Cubes.cs
+++ Assets/Scripts/Cubes.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 public class Cubes : MonoBehaviour {
 
+    private static Color32 green = new Color32(0, 180, 60, 255);
+    private static Color32 grey = new Color32(80, 80, 80, 255);
+    private static Color32 white = new Color32(255, 255, 255, 255);
+
+
     private Dictionary<string, Chunk> chunkDictionary;
 
+    private Vector2[] octaveOffsets;
     public int width;
     public int length;
     public int height;
@@ -51,8 +57,16 @@ public class Cubes : MonoBehaviour {
     }
 
     public void Generate() {
-        for (int z = 0; z < 3; z++) {
-            for (int x = 0; x < 3; x++) {
+        System.Random prng = new System.Random(2);
+        octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++) {
+            float offsetX = prng.Next(-100000, 100000) + xNoiseOffset;
+            float offsetZ = prng.Next(-100000, 100000) + zNoiseOffset;
+            octaveOffsets[i] = new Vector2(offsetX, offsetZ);
+        }
+
+        for (int z = 0; z < 4; z++) {
+            for (int x = 0; x < 4; x++) {
                 var name = "Chunk [" + x + ", " + z + "]";
                 var location = new Vector3(x * width, 0, z * length);
                 var material = GetComponent<MeshRenderer>().sharedMaterial;
@@ -82,8 +96,6 @@ public class Cubes : MonoBehaviour {
         mesh.Clear();
 
         int trianglesPerCube = 4 * 3;
-        var black = new Color32(0, 0, 0, 255);
-        var white = new Color32(255, 255, 255, 255);
 
         Vector3[] vertices = new Vector3[width * length * height * 12];
         Color32[] colors = new Color32[vertices.Length];
@@ -114,7 +126,7 @@ public class Cubes : MonoBehaviour {
 
                     Vector3[] interpolatedVertices = Triangulation.GetVertices(isoLevel, cubeIndex, cubePoints);
                     for (int v = 0; v < interpolatedVertices.Length; v++) {
-                        colors[vertexOffset + v] = Color32.Lerp(black, white, interpolatedVertices[v].y / height);
+                        colors[vertexOffset + v] = CalculateColor(interpolatedVertices[v].y / height);
                         vertices[vertexOffset + v] = interpolatedVertices[v];
                     }
 
@@ -134,31 +146,36 @@ public class Cubes : MonoBehaviour {
     }
 
     float CalculateIsoValue(Vector3 vertex) {
-        System.Random prng = new System.Random(2);
-        Vector2[] octaveOffsets = new Vector2[octaves];
-        for (int i = 0; i < octaves; i++) {
-            float offsetX = prng.Next(-100000, 100000) + xNoiseOffset;
-            float offsetZ = prng.Next(-100000, 100000) + zNoiseOffset;
-            octaveOffsets[i] = new Vector2(offsetX, offsetZ);
-        }
-
         float amplitude = 1;
         float frequency = 1;
         float noiseHeight = 0;
+        float yValue = 0;
 
         for (int i = 0; i < octaves; i++) {
             float sampleX = vertex.x * scale * frequency + octaveOffsets[i].x;
-            float sampleY = vertex.z * scale * frequency + octaveOffsets[i].y;
+            float sampleZ = vertex.z * scale * frequency + octaveOffsets[i].y;
+            float sampleY = vertex.y * scale * frequency;
 
-            float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+            float perlinValue = Mathf.PerlinNoise(sampleX, sampleZ);
             noiseHeight += perlinValue * amplitude;
+
+            float yPerlin = Mathf.PerlinNoise(sampleY, sampleY);
+            yValue += yPerlin * amplitude;
 
             amplitude *= persistance;
             frequency *= lacunarity;
         }
-        return noiseHeight * (1 - vertex.y / height);
+        // return (noiseHeight + yValue) / 2;
+        return 1 - (noiseHeight / 2 + Mathf.Sin(vertex.y / height * Mathf.PI / 2) / 2);
     }
 
+    Color32 CalculateColor(float y) {
+        if (y < .5) {
+            return Color32.Lerp(green, grey, y * 2);
+        } else {
+            return Color32.Lerp(grey, white, y * 2 - 1);
+        }
+    }
 
     Vector3[] CreateCubeVertices(Vector3 origin) {
         Vector3[] vertices = new Vector3[]{
